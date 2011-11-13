bb65f32c534622c453185499dae4f8de65759cdd
abki <amirouche.boubekki@gmail.com>
2011-11-13 18:54:54 +0100

    debug improt_class

--- client/dominus/util.py
+++ client/dominus/util.py
@@ -4,10 +4,14 @@ import exception
 def import_class(class_path):
     components = class_path.split('.')
     module_path = '.'.join(components[:-1])
-    mod = __import__(module_path, {}, {}, [])
-    for comp in components[1:]:
-        mod = getattr(mod, comp)
-    return mod
+    try:
+        mod = __import__(module_path, {}, {}, [])
+    except:
+        return None
+    else:
+        for comp in components[1:]:
+            mod = getattr(mod, comp)
+        return mod
 
 
 def handle_response(message):
